4924865df7bbc8a4a4e3f4c1793010368544a11b
Kierán Meinhardt <kmein@posteo.de>
2021-12-17 21:55:44 +0100

    WIP: R

--- shell.nix
+++ shell.nix
@@ -1,6 +1,23 @@
 { pkgs ? import <nixpkgs> {} }:
+let
+  packages = with pkgs.rPackages; [ ggplot2 ggthemes rmarkdown knitr ];
+  r = (pkgs.rWrapper.override {inherit packages;});
+in
 pkgs.mkShell {
   buildInputs = with pkgs; [
+    r
+    (pkgs.rstudioWrapper.override {inherit packages;})
+    (pkgs.writers.writeDashBin "knitr" ''
+      [ $# -eq 2 ] || exit 1
+
+      extension="$1"
+      input="$2"
+      output="$(basename "$input" .Rmd)"
+
+      [ "$extension" = html -o "$extension" = pdf ] || exit 1
+
+      ${r}/bin/R --no-restore -e "rmarkdown::render('$input',output_file='$output.$extension')" >/dev/null
+    '')
     (python3.withPackages (p: [
       p.pandas
       p.jupyter
